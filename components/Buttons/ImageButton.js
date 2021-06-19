c05809a2838a0b32c01a5ce492af7dfbcf14cb41
savitoj <savitoj.cheema@gmail.com>
2021-06-19 14:30:47 +0530

    landing page action buttons min dimensions fixed

--- components/Buttons/ImageButton.js
+++ components/Buttons/ImageButton.js
@@ -3,7 +3,11 @@ import styles from "./image-button.module.css";
 
 const ImageButton = ({ children, type, action }) => {
   return (
-    <button className={styles[type]} onClick={action}>
+    <button
+      className={styles[type]}
+      onClick={action}
+      style={{ minHeight: "20px", minWidth: "20px" }}
+    >
       {children}
     </button>
   );
