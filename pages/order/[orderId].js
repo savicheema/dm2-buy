f413dbf20c1ad7a15c39b4614962fea481919a96
Hitesh S <fxnoob71@gmail.com>
2021-07-25 20:30:19 +0530

    fixes #27 (#103)
    Co-authored-by: unknown <aditya@welivdit.com>

--- pages/order/[orderId].js
+++ pages/order/[orderId].js
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import orderStyles from './order.module.css';
-import Head from 'next/head';
-import LoaderComponent from '../../components/Loader';
+import React, { useEffect, useState } from "react";
+import orderStyles from "./order.module.css";
+import Head from "next/head";
+import LoaderComponent from "../../components/Loader";
+import Footer from "../../components/Footer";
 
 export async function getServerSideProps(context) {
   const { query } = context;
   return {
-    props: { orderId: query.orderId } // will be passed to the page component as props
+    props: { orderId: query.orderId }, // will be passed to the page component as props
   };
 }
 
 export default function Order(props) {
   const [loading, setLoading] = useState(false);
-  const [status, setStatus] = useState('');
-  const [retryLink, setRetryLink] = useState('');
+  const [status, setStatus] = useState("");
+  const [retryLink, setRetryLink] = useState("");
   const [meta, setMeta] = useState({
-    title: 'Dm 2 Buy'
+    title: "Dm 2 Buy",
   });
   const [order, setOrder] = useState();
   const { orderId } = props;
@@ -33,7 +34,7 @@ export default function Order(props) {
       setStatus(order.payment_status);
       setRetryLink(json.paymentLink);
     } catch (e) {
-      alert('something went wrong!');
+      alert("something went wrong!");
       console.log({ e });
     }
     setLoading(false);
@@ -47,11 +48,11 @@ export default function Order(props) {
   const popUpFrame = (paymentLink) => {
     const popup = window.open(
       paymentLink,
-      'newwindow',
-      'height=600,width=800, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no, directories=no, status=no'
+      "newwindow",
+      "height=600,width=800, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no, directories=no, status=no"
     );
     popup.onclose = () => {
-      console.log('Pop up close');
+      console.log("Pop up close");
     };
   };
   const openPopup = () => {
@@ -63,36 +64,36 @@ export default function Order(props) {
       <Head>
         <title>{meta.title}</title>
         <meta
-          name='description'
-          content='Check my shop out and bag my latest drop'
+          name="description"
+          content="Check my shop out and bag my latest drop"
         />
-        <link rel='icon' href='/favicon.ico' />
-        <link href='/fonts/fonts.css' rel='stylesheet' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link rel="icon" href="/favicon.ico" />
+        <link href="/fonts/fonts.css" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href='https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Roboto:wght@400;700&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
         />
         <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1, maximum-scale=1,user-scalable=0'
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1,user-scalable=0"
         />
 
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content={meta.title} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={meta.title} />
         <meta
-          property='og:description'
-          content='Check my shop out and bag my latest drop'
+          property="og:description"
+          content="Check my shop out and bag my latest drop"
         />
-        <meta property='og:image:secure' content='/favicon.ico' />
+        <meta property="og:image:secure" content="/favicon.ico" />
       </Head>
       {loading && <LoaderComponent />}
       <div className={orderStyles.containerChild}>
-        {status == 'complete' && (
+        {status == "complete" && (
           <div className={orderStyles.paymentSuccessContainer}>
             <div className={orderStyles.successDiv}>
-              <div style={{ width: '40%' }}>
-                <img src='/success-emoji.svg' />
+              <div style={{ width: "40%" }}>
+                <img src="/success-emoji.svg" />
                 <h1>Order Successful</h1>
                 <p className={orderStyles.successMessage}>
                   "Hey, Kim here. Thank you so much for buying from my shop. I
@@ -102,7 +103,7 @@ export default function Order(props) {
                 </p>
               </div>
               <div className={orderStyles.influencerImage}>
-                <img src='/kim.png' />
+                <img src="/kim.png" />
               </div>
             </div>
             <p className={orderStyles.feedbackMessage}>
@@ -138,7 +139,7 @@ export default function Order(props) {
               </p>
             </div>
             <div className={orderStyles.buyerContainer}>
-              <div style={{ width: '90%' }}>
+              <div style={{ width: "90%" }}>
                 <div style={{ fontSize: 25 }}>{order.buyer.name}</div>
                 <div style={{ marginTop: 10 }}>
                   <div>{order.address.address_line_1}</div>
@@ -154,30 +155,29 @@ export default function Order(props) {
             <div
               style={{
                 marginTop: 100,
-                display: 'flex',
-                justifyContent: 'center'
+                display: "flex",
+                justifyContent: "center",
               }}
             >
               <h2>dm2buy</h2>
             </div>
           </div>
         )}
-        {status == 'payment_pending' && (
+        {status == "payment_pending" && (
           <div className={orderStyles.paymentFailedContainer}>
             <div className={orderStyles.failedMessageContainer}>
               <div>
-                <p> Payment Failed </p>
-                <p>
+                <p className={orderStyles.errorHeader}> Payment Failed </p>
+                <p className={orderStyles.errorText}>
                   There seems to be an issue with the last transaction and your
-                  payment for ₹{order.order_total} didn’t come through
+                  payment for ₹{order.order_total} didn’t come through.
                 </p>
                 <button onClick={openPopup} className={orderStyles.retryButton}>
-                  <img src='/group.svg' />
-                  Retry
+                  Try again
                 </button>
               </div>
               <div>
-                <img src='/noun-warning-4059833.svg' />
+                <img src="/noun-warning-4059833.svg" />
               </div>
             </div>
             <div className={orderStyles.packageDetailContainer}>
@@ -195,9 +195,9 @@ export default function Order(props) {
               </div>
             </div>
             <div className={orderStyles.buyerContainer}>
-              <div style={{ width: '90%' }}>
-                <div style={{ fontSize: 25 }}>{order.buyer.name}</div>
-                <div style={{ marginTop: 10 }}>
+              <div className={orderStyles.buyerInfo}>
+                <div className={orderStyles.buyerName}>{order.buyer.name}</div>
+                <div className={orderStyles.buyerAddress}>
                   <div>{order.address.address_line_1}</div>
                   <div>{order.address.city}</div>
                   <div>{order.address.state}</div>
@@ -208,18 +208,10 @@ export default function Order(props) {
               </div>
               <div className={orderStyles.homeIcon}>🏠</div>
             </div>
-            <div
-              style={{
-                marginTop: 100,
-                display: 'flex',
-                justifyContent: 'center'
-              }}
-            >
-              <h2>dm2buy</h2>
-            </div>
           </div>
         )}
       </div>
+      <Footer />
     </div>
   );
 }
