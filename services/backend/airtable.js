26ef8c09c04fb8555571cb76125152993c1fec75
fxnoob <fxnoob71@gmail.com>
2021-07-27 07:33:26 +0530

    issue-117. add update product status api

--- services/backend/airtable.js
+++ services/backend/airtable.js
@@ -1,30 +1,47 @@
 import Airtable from "airtable";
-import { airtableBaseId as baseId  } from "../helper"
+import { airtableBaseId as baseId } from "../helper";
 
 Airtable.configure({
-    apiKey: process.env.AIRTABLE_KEY
+  apiKey: process.env.AIRTABLE_KEY,
 });
 
 const base = Airtable.base(baseId);
 
 function getRecordBySubdomain(subdomain) {
-    return new Promise((resolve, reject) => {
-        base('Stores').select({
-            view: 'Grid view',
-            filterByFormula:`{subdomain }="${subdomain}"`
-        }).firstPage(function(err, records) {
-            if (err) { console.error(err); reject(err); }
-            else {
-                const [ record ] = records
-                resolve(record? record._rawJson: null);
-            }
-        });
-    })
+  return new Promise((resolve, reject) => {
+    base("Stores")
+      .select({
+        view: "Grid view",
+        filterByFormula: `{subdomain }="${subdomain}"`,
+      })
+      .firstPage((err, records) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          const [record] = records;
+          resolve(record ? record._rawJson : null);
+        }
+      });
+  });
 }
 
+function updateProductStatus(productId, status) {
+  return new Promise((resolve, reject) => {
+    base("Products").update(
+      productId,
+      {
+        Status: status,
+      },
+      (err, record) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(record.get("Status"));
+        }
+      }
+    );
+  });
+}
 
-export {
-    base,
-    getRecordBySubdomain
-};
-
+export { base, getRecordBySubdomain, updateProductStatus };
