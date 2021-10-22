b89e6a89ef417e0fc3ef59d9569a5f4b49e10578
fxnoob <fxnoob71@gmail.com>
2021-10-22 21:54:35 +0530

    resolve pick util import

--- backend/src/middlewares/validate.js
+++ backend/src/middlewares/validate.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 const httpStatus = require('http-status');
-const pick = require('../utils/pick');
+const { pick } = require('../utils/pick');
 const ApiError = require('../utils/ApiError');
 
 const validate = (schema) => (req, res, next) => {
