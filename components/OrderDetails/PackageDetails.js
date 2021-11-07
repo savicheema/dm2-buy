ff36c7de4a31c6b6e2e1c24307cea417c26c9e38
savitoj <savitoj.cheema@gmail.com>
2021-11-07 23:19:42 +0530

    testing

--- components/OrderDetails/PackageDetails.js
+++ components/OrderDetails/PackageDetails.js
@@ -12,31 +12,35 @@ export default function PackageDetails(props) {
         <div className={orderStyles.packageIcon}>📦</div>
       </div>
 
-      {order.products.map((item) => (
-        <div className={orderStyles.orderDiv}>
-          <div>
-            {item.name} <span> &#215;</span> {item.quantity}{" "}
-            <span
-              style={{
-                backgroundColor: item.colour,
-                borderRadius: "50%",
-                height: "8px",
-                width: "8px",
-              }}
-            ></span>
-            <div className={styles.product_specs}>
-              {item.customAttributes?.map((ca) => (
-                <span>
-                  {ca.name} - {ca.value + " "}
-                </span>
-              ))}
+      {order.products.map((item) => {
+        console.log("ITEM", item);
+        return (
+          <div className={orderStyles.orderDiv}>
+            <div>
+              {item.name} <span> &#215;</span> {item.quantity}{" "}
+              <span
+                style={{
+                  display: "inline-block",
+                  backgroundColor: item.colour,
+                  borderRadius: "50%",
+                  height: "8px",
+                  width: "8px",
+                }}
+              ></span>
+              <div className={styles.product_specs}>
+                {item.customAttributes?.map((ca) => (
+                  <span>
+                    {ca.name} - {ca.value + " "}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <div className={orderStyles.orderTotal}>
+              ₹{item.quantity * item.price}{" "}
             </div>
           </div>
-          <div className={orderStyles.orderTotal}>
-            ₹{item.quantity * item.price}{" "}
-          </div>
-        </div>
-      ))}
+        );
+      })}
 
       {/* <div className={orderStyles.orderTotal}>₹{order.order_total}</div> */}
     </div>
