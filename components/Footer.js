b42f1acf299d42276337b8452a6fdee98ef19b60
dm2buy <rohankokane19@gmail.com>
2021-11-25 23:34:18 +0530

    footer and pages

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,6 @@
 import styles from "./footer.module.css";
 import { getSubDomainOfPage } from "../services/helper";
+import Link from 'next/link';
 
 const Footer = () => {
   const navigateToHome = () => {
@@ -10,7 +11,21 @@ const Footer = () => {
   return (
     <footer className={styles.footer} onClick={navigateToHome}>
       {/* <Image src="/instagram-4.png" width="24" height="24" /> */}
-      <span className={styles.tagline}>dm2buy</span>
+      <div className={styles.tagline}>dm2buy</div>
+      <div className={styles.footerLinks}>
+          <Link href="/terms">
+            Terms
+          </Link>
+          <Link href="">
+            Returns
+          </Link>
+          <Link href="">
+            Privacy Policy
+          </Link>
+          <Link href="">
+            Contact us
+          </Link>
+      </div>
     </footer>
   );
 };
