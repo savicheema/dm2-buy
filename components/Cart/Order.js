3c9b58cce90ed608d3f24c64199d570a3db3570c
dm2buy <rohankokane19@gmail.com>
2021-12-06 00:07:33 +0530

    gift code added

--- components/Cart/Order.js
+++ components/Cart/Order.js
@@ -3,6 +3,7 @@ import styles from "./order.module.css";
 import { getPrice } from "../../services/frontend/pricing.service";
 
 import OrderItem from "./OrderItem";
+import GiftCode from "./GiftCode";
 
 export default function Order({ cart, checkInputs }) {
   const {
@@ -17,56 +18,64 @@ export default function Order({ cart, checkInputs }) {
   return (
     <>
       <div className={styles.order}>
-        <h2 className={styles.orderTitle}>
-          <span>📦</span> Your Order
-        </h2>
-        <div className={styles.orderList}>
-          {cart.products.map((product, index) => (
-            <OrderItem item={product} key={index} />
-          ))}
-        </div>
-        <div className={styles.charges}>
+        <div className={styles.orderBox}>
+          <h2 className={styles.orderTitle}>
+            <span>📦</span> Your Order
+          </h2>
+          <div className={styles.orderList}>
+            {cart.products.map((product, index) => (
+              <OrderItem item={product} key={index} />
+            ))}
+          </div>
+          <div className={styles.charges}>
+              <div className={styles.orderItem}>
+                <div className={styles.productDetails}>
+                  <span className={styles.shippingEmoji}>🚛</span>
+                  <div className={styles.productName}>
+                    Shipping Fee <br />{" "}
+                    {actualShippingFee && !shippingFeeApplied ? (
+                      <div className={styles.free_shipping}>
+                        You saved
+                        {` ${String.fromCharCode(0x20b9)}${actualShippingFee}`}! 🙌
+                      </div>
+                    ): null}
+                  </div>
+                </div>
+                <div className={styles.productPrice}>
+                  {`${String.fromCharCode(0x20b9)}${shippingFee}`}
+                </div>
+              </div>
             <div className={styles.orderItem}>
               <div className={styles.productDetails}>
-                <span className={styles.shippingEmoji}>🚛</span>
-                <div className={styles.productName}>
-                  Shipping Fee <br />{" "}
-                  {actualShippingFee && !shippingFeeApplied ? (
-                    <div className={styles.free_shipping}>
-                      You saved
-                      {` ${String.fromCharCode(0x20b9)}${actualShippingFee}`}! 🙌
-                    </div>
-                  ): null}
+                <span className={styles.shippingEmoji}>💳</span>
+                <div className={styles.productName} style={{ width: "8rem" }}>
+                  Payment Processing Fee
                 </div>
               </div>
               <div className={styles.productPrice}>
-                {`${String.fromCharCode(0x20b9)}${shippingFee}`}
+                {`${String.fromCharCode(0x20b9)}${paymentProcessingFee}`}
               </div>
-            </div>
-          <div className={styles.orderItem}>
-            <div className={styles.productDetails}>
-              <span className={styles.shippingEmoji}>💳</span>
-              <div className={styles.productName} style={{ width: "8rem" }}>
-                Payment Processing Fee
-              </div>
-            </div>
-            <div className={styles.productPrice}>
-              {`${String.fromCharCode(0x20b9)}${paymentProcessingFee}`}
             </div>
           </div>
         </div>
+        <GiftCode />
+        <button
+          className={styles.orderButton}
+          onClick={async () => {
+            const isFormValid = await checkInputs();
+            if (isFormValid) {
+              localStorage.removeItem("product");
+            }
+          }}
+        >
+          <span>
+            Proceed to Pay
+          </span>
+          <span>
+            {String.fromCharCode(0x20b9) + priceWithPaymentProcessingFee}
+          </span>
+        </button>
       </div>
-      <button
-        className={styles.orderButton}
-        onClick={async () => {
-          const isFormValid = await checkInputs();
-          if (isFormValid) {
-            localStorage.removeItem("product");
-          }
-        }}
-      >
-        {`Pay — ${String.fromCharCode(0x20b9) + priceWithPaymentProcessingFee}`}
-      </button>
     </>
   );
 }
