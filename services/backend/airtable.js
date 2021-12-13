b17bea16f664832fced6feccd5ceb98cd998778d
dm2buy <rohankokane19@gmail.com>
2021-12-13 13:49:58 +0530

    condition

--- services/backend/airtable.js
+++ services/backend/airtable.js
@@ -105,7 +105,8 @@ async function validatePromoCodeFromDB(giftCode, storeName) {
             };
             let [record] = records;
             record = {...record._rawJson, ...record._rawJson.fields}
-            if(record.active==false) {
+            console.log(typeof (record.active), '<----- typeof')
+            if(record.active!='false') {
               console.log(record.active, '<----- active')
               if(record.["store_name (from Store)"].includes(storeName)){
                 console.log(record.active, '<----- active resolve')
