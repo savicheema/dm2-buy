d7fa986046d9a101522e9d00c46be8776631d27a
simmy bajaj <simmybajaj1603@gmail.com>
2022-01-12 17:55:45 +0530

    fix for bug-363

--- components/Home.js
+++ components/Home.js
@@ -1,43 +1,44 @@
-import React from 'react';
+import React from "react";
 import styles from "./home.module.css";
+import ReactPlayer from "react-player";
 
 export default class Home extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            heroMedia: '',
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      heroMedia: "",
+    };
 
-        this.videoTag = React.createRef();
-    }
+    this.videoTag = React.createRef();
+  }
 
-    componentDidMount() {
-        if (this.props.heroMedia && this.props.heroMedia.length) {
-            this.setState({heroMedia: this.props.heroMedia[0]['url']});
-        }
-        this.props.endLoading();
+  componentDidMount() {
+    if (this.props.heroMedia && this.props.heroMedia.length) {
+      this.setState({ heroMedia: this.props.heroMedia[0]["url"] });
     }
+    this.props.endLoading();
+  }
 
-    render() {
-        return (
-            !this.props.loading
-            && <div className={styles.homePage}>
-                <div
-                    className={styles.bannerContainer}
-                    // onClick={() => this.props.updateHomeActive(false)}
-                >
-                    <video
-                        ref={ref => this.videoTag = ref}
-                        className={styles.bannerImage}
-                        controls={false}
-                        autoPlay={true}
-                        muted>
-                        <source src={this.state.heroMedia} type="video/mp4" />
-                        <source src={this.state.heroMedia} type="video/ogg" />
-                        Your browser does not support the video tag.
-                    </video>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      !this.props.loading && (
+        <div className={styles.homePage}>
+          <div
+            className={styles.bannerContainer}
+            // onClick={() => this.props.updateHomeActive(false)}
+          >
+            <ReactPlayer
+              className={styles.bannerImage}
+              width={'100%'}
+              height={'calc(var(--max-width) * 1.5)'}
+              muted
+              playsinline={true}
+              playing={true}
+              url={this.state.heroMedia}
+            />
+          </div>
+        </div>
+      )
+    );
+  }
 }
