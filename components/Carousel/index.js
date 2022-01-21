bc801500117facbaf9277e35718b666259b4853c
hemalhansda <hemal.hansda25@gmail.com>
2022-01-21 18:51:22 +0530

    fixed the carousel padding issue

--- components/Carousel/index.js
+++ components/Carousel/index.js
@@ -10,7 +10,7 @@ import NextArrow from "./NextArrow";
 import Indicator from "./Indicator";
 
 const DM2BuyCarousel = ({ product }) => (
-  <div>
+  <div style={{padding: '0px 18px'}}>
     <Carousel
       showThumbs={false}
       showStatus={false}
@@ -34,7 +34,7 @@ const DM2BuyCarousel = ({ product }) => (
     >
       {product.allPhotos &&
         product.allPhotos.map((photo, index) => {
-          return <Image src={photo.url} height={425} width={340} key={index} priority/>;
+          return <Image src={photo.url} height={425} width={360} key={index} priority/>;
         })}
     </Carousel>
   </div>
