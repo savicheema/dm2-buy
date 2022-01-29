776876881966b966dffbd8f19d23f7dbc788ea42
Shubham <sgoel19922@gmail.com>
2022-01-29 11:13:00 +0530

    [add] custom attributes in update product

--- backend/src/services/contentful-management.service.js
+++ backend/src/services/contentful-management.service.js
@@ -48,25 +48,150 @@ async function updateProductById(id, product) {
         }
     }
 
+
+    let assetArray = [];
+    if(product.otherPhotos){
+        console.log("processing other images....")
+        for (let index = 0; index < product.otherPhotos.length; index++) {
+            const element = product.otherPhotos[index];
+            let assetRes = await new Promise((resolve, reject) => {
+                client.getSpace('vidnutv0ls36')
+                .then((space) => space.getEnvironment("master"))
+                .then((environment)=> environment.createAsset({
+                    fields: {
+                        title: {
+                        'en-US': product.name+"-" + convertToSlug.convertToSlug(new Date().toISOString())
+                        },
+                        description: {
+                        'en-US': product.name
+                        },
+                        file: {
+                        'en-US': {
+                            contentType: 'image/jpeg',
+                            fileName: 'example.jpeg',
+                            upload: element
+                        }
+                        }
+                    }
+                }))
+                .then((asset) => asset.processForAllLocales())
+                .then((asset) => asset.publish())
+                .then((asset) => {
+                            console.log(`processing ${index} image done....`)
+                            resolve(asset)
+                        })
+                .catch(error => reject(error))
+            });
+
+            var assetObj = {
+                sys:{
+                    "type": "Link",
+                    "linkType": "Asset",
+                    "id": assetRes.sys.id
+                }
+            }
+            assetArray.push(assetObj)   
+        }
+    }
+    var customAttributeArray = []
+    if(product.customAttributes){
+        for (let index = 0; index < product.customAttributes.length; index++) {
+            const element = product.customAttributes[index];
+            console.log(element)
+            let customAttributeRes = await  new Promise((resolve, reject) => {
+
+                client.getSpace('vidnutv0ls36')
+                .then((space) => space.getEnvironment("master"))
+                .then((environment) => environment.createEntry('customAttributes',{
+                    fields: {
+                        name: {
+                            'en-US': element.name
+                        },
+                        attributeType:{
+                            'en-US': convertToSlug.convertToSlug(element.attributeType)
+                        },
+                        required:{
+                            'en-US': element.required
+                        }
+                    }
+                }))
+                .then((entry)=> entry.publish())
+                .then((entry)=>  {
+                    console.log(entry)
+                    resolve(entry)
+                })
+                .catch(err => {
+                    console.log('contentful err: ', err);
+                    reject(err);
+                });
+            });
+
+            var custAttrObj = {
+                sys:{
+                    "type": "Link",
+                    "linkType": "Entry",
+                    "id": customAttributeRes.sys.id
+                }
+            }
+            customAttributeArray.push(custAttrObj) 
+        }
+    }
+
     return new Promise((resolve, reject) => {
         console.log(client)
         client.getSpace('vidnutv0ls36')
             .then((space) => space.getEnvironment("master"))
             .then((environment) => environment.getEntry(id))
             .then(entry => {
-                console.log(entry)
-                if(product.name)
-                    entry.fields.name['en-US'] = product.name
-                if(product.price)
-                    entry.fields.price['en-US'] = product.price
-                if(product.availableStock)
-                    entry.fields.availableStock['en-US'] = product.availableStock
-                if(product.description)
-                    entry.fields.description['en-US'] = product.description
-                if(assetHeaderImageObj)
-                    entry.fields.headerPhoto['en-US'] = assetHeaderImageObj
+                // console.log(entry)
+                 // if(assetArray.length>0)
+                //     entry.fields.otherPhotos['en-US'] = [...assetArray]
+                //entry.update()
                 
-                entry.update()
+                let patchArray = []
+                if(product.name)
+                    patchArray.push({
+                        op: 'add',
+                        path: '/fields/name',
+                        value:{'en-US':product.name} 
+                    });
+                if(product.price)
+                    patchArray.push({
+                        op: 'add',
+                        path: '/fields/price',
+                        value:{'en-US':product.price} 
+                    });
+                if(product.description)
+                    patchArray.push({
+                        op: 'add',
+                        path: '/fields/description',
+                        value:{'en-US':product.description} 
+                    });
+                if(product.availableStock)
+                    patchArray.push({
+                        op: 'add',
+                        path: '/fields/availableStock',
+                        value:{'en-US':product.availableStock} 
+                    });
+                if(assetHeaderImageObj)
+                    patchArray.push({
+                        op: 'add',
+                        path: '/fields/headerPhoto',
+                        value:{'en-US':assetHeaderImageObj} 
+                    });
+                if(assetArray.length>0)
+                    patchArray.push({
+                        op: 'add',
+                        path: '/fields/otherPhotos',
+                        value:{'en-US':[...assetArray]} 
+                    });
+                if(customAttributeArray.length>0)
+                    patchArray.push({
+                        op:'add',
+                        path: '/fields/customAttributes',
+                        value:{'en-US':[...customAttributeArray]}
+                    })
+                entry.patch([...patchArray])
                 .then((entry)=> resolve(entry.publish()))
             })
             .catch(err => {
