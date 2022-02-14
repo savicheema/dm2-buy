8b4e02a217119fb888fa8a4f8fbd9c88e72399ce
hemalhansda <hemal.hansda25@gmail.com>
2022-02-14 21:30:53 +0530

    fixed order placing bug

--- services/frontend/pricing.service.js
+++ services/frontend/pricing.service.js
@@ -34,7 +34,7 @@ const getPrice = (cart) => {
   const processingFee = Number((calculatedPrice * 0.02).toFixed(2));
 
   calculatedPrice += processingFee;
-  calculatedPrice = calculatedPrice.toFixed(2);
+  calculatedPrice = calculatedPrice != 0 ? calculatedPrice.toFixed(2) : calculatedPrice;
 
   return {
     productTotalPrice: productTotalPrice,
