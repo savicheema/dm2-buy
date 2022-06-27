d84fff9c1b251560648f9d187370da076e13ece4
Hemaal Taras Hansda <hemal.hansda25@gmail.com>
2022-06-27 12:01:28 +0530

    fixed the typo and font

--- components/Cart/index.js
+++ components/Cart/index.js
@@ -185,7 +185,7 @@ const Cart = ({ cart, store, applyPromoCode, removePromoCode }) => {
       />
       <Toast
         type="error"
-        message={<span style={{fontFamily: 'Roboto'}}>Minimum order about on our site is<br/>Rs. {store?.paymentInfo?.minimumCartValue}. — Sorry about that :(</span>}
+        message={<span style={{fontFamily: '"Roboto", sans-serif'}}>Minimum order amount on our site is<br/>Rs. {store?.paymentInfo?.minimumCartValue}. — Sorry about that :(</span>}
         open={minimumCartError}
       />
     </div>
